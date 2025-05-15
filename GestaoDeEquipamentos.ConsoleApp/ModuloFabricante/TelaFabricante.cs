be84f1340f6a014149a4f659a717a58c2a37aba0
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-05-15 20:04:12 -0300

    Implementada validação recursiva

--- GestaoDeEquipamentos.ConsoleApp/ModuloFabricante/TelaFabricante.cs
+++ GestaoDeEquipamentos.ConsoleApp/ModuloFabricante/TelaFabricante.cs
@@ -51,6 +51,8 @@ public class TelaFabricante
             Console.Write("\nDigite ENTER para continuar...");
             Console.ReadLine();
 
+            CadastrarRegistro();
+
             return;
         }
 
